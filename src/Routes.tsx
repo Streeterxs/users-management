47b02926ec98809b7f31665e82593691f3000d49
Streeterxs <afonso_mg3@hotmail.com>
2020-06-17 12:23:53 -0300

    Routes - Prodecting routes with logged in or logged out logic.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import { Login, UserEdit, UserCreation, UserList } from './Pages';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const Routes = () => (
-    <Switch>
-        <Route path="/" exact component={UserList}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/create" component={UserCreation}/>
-        <Route path="/edit/:identifier" component={UserEdit}/>
-    </Switch>
-);
+import { Login, UserEdit, UserCreation, UserList } from './Pages';
+import { RootState } from './Store/Store';
+
+const Routes = () => {
+    const {coreReducer} = useSelector((state: RootState) => state);
+
+    const PrivateRoute = ({redirectTo, authLogic, component: Component, ...rest }: any) => (
+        <Route {...rest} render={(props) => (
+            authLogic
+            ? <Component {...props}/>
+            : <Redirect to={redirectTo} />
+        )} />
+      )
+
+    return (
+        <Switch>
+            <PrivateRoute authLogic={coreReducer.isLogged} redirectTo="/login" path="/" exact component={UserList} />
+            <PrivateRoute authLogic={!coreReducer.isLogged} redirectTo="/" path="/login" component={Login} />
+            <PrivateRoute authLogic={coreReducer.isLogged} redirectTo="/login" path="/create" component={UserCreation} />
+            <PrivateRoute authLogic={coreReducer.isLogged} redirectTo="/login" path="/edit/:identifier" component={UserEdit} />
+        </Switch>
+    );
+};
 
 export default Routes;
